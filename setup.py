f1e6291e4d6354761a95cbb72b74c7a73121eb7f
mukesh1236 <mukesh.malipeddi@gmail.com>
2021-03-17 11:09:22 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ineuron wafer classification',
+    author='mukesh',
+    license='MIT',
+)
